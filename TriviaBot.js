22d4d343a70b5f3b7239732b5dba4f82b97cf9c8
Mahfuj Hussain <mahfujhussain284@gmail.com>
2025-06-17 10:01:34 +0600

    After solve added TriviaBot Task on github

---
+++ TriviaBot.js
@@ -0,0 +1,25 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName ="Rubby";
+let botLocation = "Rajshahi";
+let favoriteLanguage = "JS";
+
+
+console.log("My name is " + botName + " and I live on "+botLocation+".");
+
+
+console.log("My favorite programming language is "+favoriteLanguage+".");
+
+let codingFact = favoriteLanguage + " was created in just 10 days in 1995!";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " runs directly in your browser — no compiler needed!";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + "  is single-threaded — it executes code one at a time!";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
