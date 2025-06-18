ca948271e762e54189d0453a90d7cb15bdef133a
Mahfuj Hussain <mahfujhussain284@gmail.com>
2025-06-18 22:49:35 +0600

    Add variable & string part sentense maker.

---
+++ sentenseMaker.js
@@ -0,0 +1,25 @@
+let adjective = "small";
+let noun = "cat";
+let verb = "sad";
+let place = "house";
+let adjective2 = "funny";
+let noun2 = "fish";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("First story: " + firstStory);
+
+
+adjective = "happy";
+noun = "boy";
+verb = "tired";
+place = "school";
+adjective2 = "big";
+noun2 = "banana";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story: " + secondStory);
+
+
+// Story created using ChatGPT
